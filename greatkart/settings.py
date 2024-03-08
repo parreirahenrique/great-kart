faa5d7f427ae0ee9feae8a0af0989efd4b4192ba
Henrique Castro <henrique_castro97@hotmail.com>
2024-03-08 14:44:05 -0300

    added cname to allowed hosts

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-greatkart-env.eba-dzrdacgj.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
