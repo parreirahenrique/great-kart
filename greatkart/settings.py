5ab744bc9813f909fdbebe4647a5b96dc8e6aeea
Henrique Castro <henrique_castro97@hotmail.com>
2024-03-20 14:10:48 -0300

    making changes to deploy to render

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'django-greatkart-env.eba-dzrdacgj.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'django-greatkart-env.eba-dzrdacgj.us-west-2.elasticbeanstalk.com', 'mysite-kw9s.onrender.com']
 
 
 # Application definition
